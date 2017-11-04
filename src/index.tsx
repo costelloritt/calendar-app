f86559d5613a8a84fd64f8482c325daae48ee8ac
Unknown <costello.ritt@gmail.com>
2017-11-03 19:48:42 -0500

    refactor MonthHeader component

--- src/index.tsx
+++ src/index.tsx
@@ -2,64 +2,65 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import Grid from './scenes/calendar-home/components/grid';
+import MonthHeader from './scenes/calendar-home/components/month-header';
 
-interface MonthChangeButtonProps {
-  id: string
-  content: string
-  clickHandler: any
-}
+// interface MonthChangeButtonProps {
+//   id: string
+//   content: string
+//   clickHandler: any
+// }
+//
+// function MonthChangeButton (props: MonthChangeButtonProps) {
+//   return(
+//     <button className="month-change-button" id={props.id} onClick={props.clickHandler}>{props.content}</button>
+//   );
+// }
 
-function MonthChangeButton (props: MonthChangeButtonProps) {
-  return(
-    <button className="month-change-button" id={props.id} onClick={props.clickHandler}>{props.content}</button>
-  );
-}
-
-interface MonthHeaderState {
-  currentMonth: string,
-  currentMonthStartDayIndex: number
-}
-
-class MonthHeader extends React.Component <any, MonthHeaderState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      currentMonth: getCurrentMonth(),
-      currentMonthStartDayIndex: getDaysCurrentMonth()
-    };
-  }
-
-  handleClick(navButtonID: string) {
-    var navDir;
-    var currentStateCopy: string;
-    var newMonth: string;
-
-    if (navButtonID === "prev-month") {
-      navDir = -1;
-    }
-    else
-      navDir = 1;
-
-    newMonth = getMonth(navDir, this.state.currentMonth);
-
-    currentStateCopy = this.state.currentMonth;
-    currentStateCopy = newMonth;
-
-    this.setState({
-      currentMonth: currentStateCopy
-    });
-  }
-
-  render() {
-    return(
-      <div className="monthHeader">
-        <MonthChangeButton content="Prev" id="prev-month" clickHandler={() => this.handleClick("prev-month")}/>
-        {this.state.currentMonth}
-        <MonthChangeButton content="Next" id="next-month" clickHandler={() => this.handleClick("next-month")}/>
-      </div>
-    );
-  }
-}
+// interface MonthHeaderState {
+//   currentMonth: string,
+//   currentMonthStartDayIndex: number
+// }
+//
+// class MonthHeader extends React.Component <any, MonthHeaderState> {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = {
+//       currentMonth: getCurrentMonth(),
+//       currentMonthStartDayIndex: getDaysCurrentMonth()
+//     };
+//   }
+//
+//   handleClick(navButtonID: string) {
+//     var navDir;
+//     var currentStateCopy: string;
+//     var newMonth: string;
+//
+//     if (navButtonID === "prev-month") {
+//       navDir = -1;
+//     }
+//     else
+//       navDir = 1;
+//
+//     newMonth = getMonth(navDir, this.state.currentMonth);
+//
+//     currentStateCopy = this.state.currentMonth;
+//     currentStateCopy = newMonth;
+//
+//     this.setState({
+//       currentMonth: currentStateCopy
+//     });
+//   }
+//
+//   render() {
+//     return(
+//       <div className="monthHeader">
+//         <MonthChangeButton content="Prev" id="prev-month" clickHandler={() => this.handleClick("prev-month")}/>
+//         {this.state.currentMonth}
+//         <MonthChangeButton content="Next" id="next-month" clickHandler={() => this.handleClick("next-month")}/>
+//       </div>
+//     );
+//   }
+// }
 
 class Calendar extends React.Component<any, any> {
 
@@ -76,55 +77,55 @@ class Calendar extends React.Component<any, any> {
 ReactDOM.render(<Calendar />, document.getElementById('root'));
 
 
-function getCurrentMonth() {
-  var monthNames = ["January", "February", "March", "April", "May", "June",
-                    "July", "August", "September", "October", "November",
-                    "December"];
-
-  var currentDate = new Date();
-  var currentMonth = monthNames[currentDate.getMonth()];
-
-  return currentMonth;
-}
-
-function getMonth(navDirection: number, currentMonth: string) {
-  var monthNames = ["January", "February", "March", "April", "May", "June",
-                    "July", "August", "September", "October", "November",
-                    "December"];
-
-  var newMonth = "";
-
-  for (let i = 0; i < monthNames.length; i++) {
-    if (currentMonth === monthNames[i]) {
-      if (i === 0 && navDirection === -1) {
-        i = 11;
-        newMonth = monthNames[i];
-      }
-      else if (i === 11 && navDirection === 1) {
-        i = 0;
-        newMonth = monthNames[i];
-      }
-      else
-        newMonth = monthNames[i + navDirection];
-      break;
-    }
-  }
-
-    return newMonth;
-}
-
-function getDaysCurrentMonth() {
-
-  //var weekDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  var date = new Date();
-  var date1 = new Date(date.getFullYear(), date.getMonth(), 1);
-
-  var firstDayOfWeek = date1.getDay();
-
-  return firstDayOfWeek;
-
-}
+// function getCurrentMonth() {
+//   var monthNames = ["January", "February", "March", "April", "May", "June",
+//                     "July", "August", "September", "October", "November",
+//                     "December"];
+//
+//   var currentDate = new Date();
+//   var currentMonth = monthNames[currentDate.getMonth()];
+//
+//   return currentMonth;
+// }
+//
+// function getMonth(navDirection: number, currentMonth: string) {
+//   var monthNames = ["January", "February", "March", "April", "May", "June",
+//                     "July", "August", "September", "October", "November",
+//                     "December"];
+//
+//   var newMonth = "";
+//
+//   for (let i = 0; i < monthNames.length; i++) {
+//     if (currentMonth === monthNames[i]) {
+//       if (i === 0 && navDirection === -1) {
+//         i = 11;
+//         newMonth = monthNames[i];
+//       }
+//       else if (i === 11 && navDirection === 1) {
+//         i = 0;
+//         newMonth = monthNames[i];
+//       }
+//       else
+//         newMonth = monthNames[i + navDirection];
+//       break;
+//     }
+//   }
+//
+//     return newMonth;
+// }
+//
+// function getDaysCurrentMonth() {
+//
+//   //var weekDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//
+//   var date = new Date();
+//   var date1 = new Date(date.getFullYear(), date.getMonth(), 1);
+//
+//   var firstDayOfWeek = date1.getDay();
+//
+//   return firstDayOfWeek;
+//
+// }
 
 
 
---
+++ src/scenes/calendar-home/components/month-header/index.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+
+interface MonthChangeButtonProps {
+  id: string
+  content: string
+  clickHandler: any
+}
+
+function MonthChangeButton (props: MonthChangeButtonProps) {
+  return(
+    <button className="month-change-button" id={props.id} onClick={props.clickHandler}>{props.content}</button>
+  );
+}
+
+interface MonthHeaderState {
+  currentMonth: string,
+  currentMonthStartDayIndex: number
+}
+
+class MonthHeader extends React.Component <any, MonthHeaderState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentMonth: getCurrentMonth(),
+      currentMonthStartDayIndex: getDaysCurrentMonth()
+    };
+  }
+
+  handleClick(navButtonID: string) {
+    var navDir;
+    var currentStateCopy: string;
+    var newMonth: string;
+
+    if (navButtonID === "prev-month") {
+      navDir = -1;
+    }
+    else
+      navDir = 1;
+
+    newMonth = getMonth(navDir, this.state.currentMonth);
+
+    currentStateCopy = this.state.currentMonth;
+    currentStateCopy = newMonth;
+
+    this.setState({
+      currentMonth: currentStateCopy
+    });
+  }
+
+  render() {
+    return(
+      <div className="monthHeader">
+        <MonthChangeButton content="Prev" id="prev-month" clickHandler={() => this.handleClick("prev-month")}/>
+        {this.state.currentMonth}
+        <MonthChangeButton content="Next" id="next-month" clickHandler={() => this.handleClick("next-month")}/>
+      </div>
+    );
+  }
+}
+
+export default MonthHeader;
+
+function getCurrentMonth() {
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+                    "July", "August", "September", "October", "November",
+                    "December"];
+
+  var currentDate = new Date();
+  var currentMonth = monthNames[currentDate.getMonth()];
+
+  return currentMonth;
+}
+
+function getMonth(navDirection: number, currentMonth: string) {
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+                    "July", "August", "September", "October", "November",
+                    "December"];
+
+  var newMonth = "";
+
+  for (let i = 0; i < monthNames.length; i++) {
+    if (currentMonth === monthNames[i]) {
+      if (i === 0 && navDirection === -1) {
+        i = 11;
+        newMonth = monthNames[i];
+      }
+      else if (i === 11 && navDirection === 1) {
+        i = 0;
+        newMonth = monthNames[i];
+      }
+      else
+        newMonth = monthNames[i + navDirection];
+      break;
+    }
+  }
+
+    return newMonth;
+}
+
+function getDaysCurrentMonth() {
+
+  //var weekDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  var date = new Date();
+  var date1 = new Date(date.getFullYear(), date.getMonth(), 1);
+
+  var firstDayOfWeek = date1.getDay();
+
+  return firstDayOfWeek;
+
+}
